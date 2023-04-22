fd5eb361d0c21edc6d03d5e665dd1e1620c48628
Jorge Bobrek <jrbobrek@gmail.com>
2023-04-22 14:04:32 -0500

    Initial Commit

---
+++ MeLi Challenge/MeLi_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeLi_ChallengeApp.swift
+//  MeLi Challenge
+//
+//  Created by Jorge Bobrek on 22/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MeLi_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MeLi ChallengeUITests/MeLi_ChallengeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MeLi_ChallengeUITestsLaunchTests.swift
+//  MeLi ChallengeUITests
+//
+//  Created by Jorge Bobrek on 22/04/23.
+//
+
+import XCTest
+
+final class MeLi_ChallengeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
